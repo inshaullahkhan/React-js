9fd933541082f5d6959de8538d0ed117716b66cc
inshaullah <inshaullah733@gmail.com>
2022-11-11 15:51:13 +0500

    intro changes

---
+++ my-app/src/PortfolioWebsite/Components/Intorduction/Intro.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Intro.css'
+
+function Intro() {
+  return (
+    <div>
+
+        <div className="introduction">
+
+        <div className="i-left">
+
+        <div className="i-name">
+            <span>Hey! I am</span>
+            <span>Insha Kundi</span>
+            <span>Front React Developerd with heigh level of Experience in web designing and devlopment producting the high quality work</span>
+        </div>
+            
+
+        <button className="button i-button">Hire Me</button>
+        <div className="i-icons">
+        <img src="./image/" alt="boy" />
+
+        
+        </div>
+        </div>
+
+        <div className="i-right">
+
+         <h1>hello this is me</h1>
+
+        </div>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Intro
